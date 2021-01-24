0e1a0632728c7a0cc8a434ec0cc8492dee94cb30
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-01-23 21:35:53 -0800

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,8 +12,16 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(IVRY).then(cache => {
       cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
     })
   )
 })
