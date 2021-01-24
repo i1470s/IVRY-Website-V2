d9cdd4c7caf010d113bd8af756d68ce9c2125995
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-01-23 22:26:51 -0800

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,20 +8,21 @@ const assets = [
   "/data/js/app.js",
   "/data/icon.png",
   "/data/icon-512.png"
-]
+];
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open('static').then(function(cache) {
-      cache.addAll(assets)
+    caches.open('static')
+    .then(function(cache) {
+        return cache.addAll(assets);
     })
-  )
-})
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
       return res || fetch(fetchEvent.request)
     })
-  )
-})
+  );
+});
