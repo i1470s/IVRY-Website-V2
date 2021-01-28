29b08dacb6e546b587194bb5a08097a47c4a3d35
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-01-27 19:03:52 -0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,13 +6,3 @@ if ("serviceWorker" in navigator) {
         .catch(err => console.log("service worker not registered", err))
     })
   }
-
-const isIos = () => {
-  const userAgent = window.navigator.userAgent.toLowerCase();
-  return /iphone|ipad|ipod/.test( userAgent );
-}
-const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-if (isIos() && !isInStandaloneMode()) {
-  this.setState({ showInstallMessage: true });
-}
--- serviceWorker.js
+++ serviceWorker.js
@@ -29,3 +29,14 @@ self.addEventListener('install', event => {
     })
   );
 });
+
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
