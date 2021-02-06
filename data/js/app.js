7fd51658a37381dccdb7c60fdc59a3c88a480a67
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-02-05 16:38:37 -0800

    Add files via upload

--- data/js/app.js
+++ data/js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("//serviceWorker.js")
+        .register("./serviceWorker.js")
         .then(res => console.log("service worker CACHE registered"))
         .catch(err => console.log("service worker CACHE not registered", err))
     });
