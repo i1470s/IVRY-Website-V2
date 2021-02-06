18803478552c3682fdc204249052439ec1ebf203
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-02-05 16:42:45 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("serviceWorker.js")
+        .then(res => console.log("service worker CACHE registered"))
+        .catch(err => console.log("service worker CACHE not registered", err))
+    });
+  };
