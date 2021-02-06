fc400c6152457b6de86d13048bd88de98d703d41
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-02-05 16:41:07 -0800

    Delete app.js

--- data/js/app.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("serviceWorker.js")
-        .then(res => console.log("service worker CACHE registered"))
-        .catch(err => console.log("service worker CACHE not registered", err))
-    });
-  };
