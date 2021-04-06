cbd033398b0bfb10316834e9e7a1c554b6ade20c
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-04-06 00:22:42 -0700

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,55 +6,62 @@
  |_____|     \_/     |____| |___||______|
   Created by i1470s#0396 Welcome :)*/
   
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
-
-self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
+  var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+  var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+  var CACHE_NAME = APP_PREFIX + VERSION
+  var URLS = [                            // Add URL you want to cache in this list.
+    '/',                     // If you have separate JS/CSS files,
+    '.',
+    '/index.html'            // add path to those files here
+  ]
   
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
-  
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetch', event.request.url);
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
+  // Respond with cached resources
+  self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
         }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
-  }
-});
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
+  
+  // Cache resources
+  self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(URLS)
+      })
+    )
+  })
+  
+  // Delete outdated caches
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create white list
+        var cacheWhitelist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX)
+        })
+        // add current cache name to white list
+        cacheWhitelist.push(CACHE_NAME)
+  
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] )
+            return caches.delete(keyList[i])
+          }
+        }))
+      })
+    )
+  })
