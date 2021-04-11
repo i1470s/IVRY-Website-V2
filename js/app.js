e3923e060619ef1bad596e3cf23df0ce23560e6e
Brayden Viana <i1470s@users.noreply.github.com>
2021-04-10 17:06:31 -0700

    app.js

--- js/app.js
+++ js/app.js
@@ -10,9 +10,3 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('serviceWorker.js');
 }
   
-document.querySelector('#show').addEventListener('click', () => {
-    const iconUrl = document.querySelector('select').selectedOptions[0].value;
-    let imgElement = document.createElement('img');
-    imgElement.src = iconUrl;
-    document.querySelector('#container').appendChild(imgElement);
-});
