aade434b22dee08c6cf9853cedb76a983b339fdf
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-05 00:41:31 -0700

    Add files via upload

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -33,11 +33,9 @@ const HomePage = () => {
         </body>
         <footer className="App-footer">
             <br></br>
-            <a href="https://discord.com/invite/ppn2U99"><img width="30px" height="30px" alt="Discord" src={discord}/></a>⠀
-            <a href="https://www.instagram.com/Ivry_Network/"><img width="30px" height="30px" alt="Instagram" src={instagram}/></a>⠀ 
-            <a href="https://www.twitter.com/Brayden_viana"><img width="30px" height="30px" alt="Twitter" src={twitter}/></a>⠀ 
-            <a href="https://www.facebook.com/brayden.viana/"><img width="30px" height="30px" alt="Facebook" src={facebook}/></a><br></br><br></br>  
-            <b>IVRY 2021 All Rights Reserved</b><br></br><br></br>
+                <a href="/home"><img width="30px" height="40px" alt="home" src={home}/></a>⠀⠀
+                <a href="/store"><img width="30px" height="40px" alt="store" src={store}/></a>⠀⠀
+                <a href="/search"><img width="30px" height="40px" alt="search" src={search}/></a>⠀⠀
         </footer>
       </div>
     );
