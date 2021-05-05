9d6f7f10e7921ac22165c0dcfa8489a8402cbdee
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-05 00:53:42 -0700

    Add files via upload

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,7 +18,7 @@ const HomePage = () => {
       <div className="App">
         <div className="nav">
           <header className="App-header">
-                <b style={{color: "#fb5804"}}>IVRY</b> <b style={{color: "#fff",fontSize: "10px"}}>V.3</b>
+                <b style={{ fontSize: "30px", color: "#fb5804"}}>IVRY</b> <b style={{color: "#fff", fontSize: "10px"}}>V.3</b>
           </header>
         </div>
         <body className="App-body">
@@ -26,7 +26,6 @@ const HomePage = () => {
           <b><p>Welcome to IVRY</p></b>
         </body>
         <footer className="App-footer">
-            <br></br>
                 <a href="/home"><img width="30px" height="40px" alt="home" src={home}/></a>⠀⠀
                 <a href="/store"><img width="30px" height="40px" alt="store" src={store}/></a>⠀⠀
                 <a href="/search"><img width="30px" height="40px" alt="search" src={search}/></a>
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -5,10 +5,6 @@ import React from "react";
 import home from '../components/images/home.svg';
 import store from '../components/images/store.svg';
 import search from '../components/images/search.svg';
-import discord from '../components/images/icon-discord.png';
-import instagram from '../components/images/icon-instagram.png';
-import twitter from '../components/images/icon-twitter.png';
-import facebook from '../components/images/icon-facebook.png';
 
 /*CSS Imports*/
 import '../components/css/App.css';
@@ -32,13 +28,11 @@ const SearchPage = () => {
                 <b><p>Coming Soon</p></b>
             </body>
             <footer className="App-footer">
-                <br></br>
-                <a href="https://discord.com/invite/ppn2U99"><img width="30px" height="30px" alt="Discord" src={discord}/></a>⠀
-                <a href="https://www.instagram.com/Ivry_Network/"><img width="30px" height="30px" alt="Instagram" src={instagram}/></a>⠀ 
-                <a href="https://www.twitter.com/Brayden_viana"><img width="30px" height="30px" alt="Twitter" src={twitter}/></a>⠀ 
-                <a href="https://www.facebook.com/brayden.viana/"><img width="30px" height="30px" alt="Facebook" src={facebook}/></a><br></br><br></br>  
-                <b>IVRY 2021 All Rights Reserved</b><br></br><br></br>
-            </footer>
+            <br></br>
+                <a href="/home"><img width="30px" height="40px" alt="home" src={home}/></a>⠀⠀
+                <a href="/store"><img width="30px" height="40px" alt="store" src={store}/></a>⠀⠀
+                <a href="/search"><img width="30px" height="40px" alt="search" src={search}/></a>
+        </footer>
         </div>
     );
 };
