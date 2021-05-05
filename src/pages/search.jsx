2e3ce134ae547e686982302530e892d491d2cc7f
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-05 01:02:15 -0700

    Add files via upload

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -18,9 +18,7 @@ const SearchPage = () => {
         <div className="App">
             <div className="nav">
             <header className="App-header">
-                <a href="/home"><img width="30px" height="40px" alt="home" src={home}/></a>⠀⠀
-                <a href="/store"><img width="30px" height="40px" alt="store" src={store}/></a>⠀⠀
-                <a href="/search"><img width="30px" height="40px" alt="search" src={search}/></a>⠀⠀
+                <b style={{ fontSize: "30px", color: "#fb5804"}}>IVRY</b> <b style={{color: "#fff", fontSize: "10px"}}>V.3</b>
             </header>
             </div>            
             <body className="App-body">
@@ -28,7 +26,6 @@ const SearchPage = () => {
                 <b><p>Coming Soon</p></b>
             </body>
             <footer className="App-footer">
-            <br></br>
                 <a href="/home"><img width="30px" height="40px" alt="home" src={home}/></a>⠀⠀
                 <a href="/store"><img width="30px" height="40px" alt="store" src={store}/></a>⠀⠀
                 <a href="/search"><img width="30px" height="40px" alt="search" src={search}/></a>
