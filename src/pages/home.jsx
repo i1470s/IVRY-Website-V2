9ad6649c519b05bdf36a93762df46eeb243510c2
Brayden Viana <i1470s@users.noreply.github.com>
2021-05-06 10:30:25 -0700

    home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -33,6 +33,12 @@ const HomePage = () => {
                 <h1>Home</h1>
                 <b><p>Welcome to IVRY</p></b>
                 <b><p>Were a group of Hackers / Coders learning new languages, and algorithms everyday, we offer a wide range of applications, products and services available to you in our shop and services sections. Whilst this is a website for IVRY and all our work, it is also my personal website if you have any questions or need support feel free to contact us at one of our socials linked at the bottem of the page, have a great day, and again welcome. IVRY Est. 1-24-2018</p></b>
+                <b><p>test</p></b>
+                <b><p>test</p></b>
+                <b><p>test</p></b>
+                <b><p>test</p></b>
+                <b><p>test</p></b>
+                <b><p>test</p></b>
           </body>
         </div>
         
