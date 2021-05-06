3efe7b08501e6684ef990b2f5a0e1727e90b027f
Brayden Viana <i1470s@users.noreply.github.com>
2021-05-06 09:55:32 -0700

    index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,6 +26,11 @@ const MainPage = () => {
             <div class="loader-wrapper">
                 <span class="loader"><span class="loader-inner"></span></span>
             </div>
+            <script>
+            $(window).on("load",function(){
+                 $(".loader-wrapper").fadeOut("slow");
+            });
+            </script>
           </body>
         </div>
       </div>
