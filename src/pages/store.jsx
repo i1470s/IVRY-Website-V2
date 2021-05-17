851dc144dedad5dd11e0977ab613d0b6edc6d349
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-16 21:45:31 -0700

    Add files via upload

--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -52,7 +52,7 @@ const StorePage = () => {
                       <br/>
 
                       <h3 style={{color: "#fb5804"}}>Store Policy</h3>
-                      <small>Welcome to our store, all our pices are in CAD(Canadian), we use sellix.ios secure and fast payment gateway for everyone of our products to help stop fraud, 
+                      <small>Welcome to our store, all our pices are in CAD(Canadian), we use sellix.io for secure and fast payment gateway for everyone of our products to help stop fraud, 
                       <br/>with over 30+ purchases in the past 3 months we think weve gained the trust of many 
                       <br/>and are able to show our selfs as fast, safe and lastly reliable.</small>
 
