e552dc6f05982fb6cc81ba71a1d46cbb26bed7e8
Brayden Viana <i1470s@users.noreply.github.com>
2021-05-20 11:27:45 -0700

    dm.svg

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,8 @@ import home from '../components/images/home.svg';
 import store from '../components/images/store.svg';
 import search from '../components/images/search.svg';
 import learn from '../components/images/learn.svg';
-import user from '../components/images/user.svg'
+import user from '../components/images/user.svg';
+import dm from '../components/images/dm.svg'
 
 /*CSS Imports*/
 import '../components/css/App.css';
@@ -23,7 +24,7 @@ const HomePage = () => {
         
         <div id="Nav-bar" className="nav">
           <header className="App-header">
-                <a href="https://ivry.tk/"><b style={{float: "left", fontSize: "30px", color: "#fb5804"}}>⠀IVRY</b></a> <a style={{float: "right", margin: "10px"}} href="/user"><img width="30px" height="30px" alt="search" src={user}/></a>
+                <a href="https://ivry.tk/"><b style={{float: "left", fontSize: "30px", color: "#fb5804"}}>⠀IVRY</b></a> <a style={{float: "right", margin: "10px"}} href="/user"><img width="30px" height="30px" alt="dm" src={dm}/></a> <a style={{float: "right", margin: "10px"}} href="/user"><img width="30px" height="30px" alt="user" src={user}/></a>  
           </header>
         </div>
         
@@ -51,7 +52,7 @@ const HomePage = () => {
         <div id="Footer">
           <footer className="App-footer">
                 <a href="/home"><img style={{opacity: ".5"}} width="30px" height="40px" alt="home" src={home}/></a>⠀⠀⠀⠀⠀⠀
-                <a href="/learn"><img width="30px" height="40px" alt="search" src={learn}/></a>⠀⠀⠀⠀⠀⠀
+                <a href="/learn"><img width="30px" height="40px" alt="learn" src={learn}/></a>⠀⠀⠀⠀⠀⠀
                 <a href="/store"><img width="30px" height="40px" alt="store" src={store}/></a>⠀⠀⠀⠀⠀⠀
                 <a href="/search"><img width="30px" height="40px" alt="search" src={search}/></a>
           </footer>
