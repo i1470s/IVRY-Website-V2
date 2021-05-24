0cee70996cc133111cb1a12889fd182985b35938
Brayden Viana <i1470s@users.noreply.github.com>
2021-05-23 20:04:37 -0700

    home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -35,7 +35,7 @@ const HomePage = () => {
                 <br/>
                 <br/>
                 <br/>
-                <h1 style={{color: "#fb5804", position: "absolute", left: "75px", top: "25px"}}>Home</h1>
+                <h1 style={{color: "#fb5804", position: "absolute", left: "10px", top: "25px"}}>Home</h1>
                 <h3>Welcome</h3>
                 <br/>
                 <small>Were IVRY, a small group of coders offering many different services and programs, varying from simple DLL injection, to GTAV modded accounts, 
