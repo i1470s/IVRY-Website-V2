981469e8ce50ad93d334d00f90100fe1571f6980
Brayden Viana <i1470s@users.noreply.github.com>
2021-05-24 09:42:16 -0700

    user

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -33,7 +33,7 @@ const SearchPage = () => {
                       <br/>
                       <br/>
                       <br/>
-                      <input style={{position: "fixed", top: "85px"}} id="search" type="search" placeholder="Search..." autofocus required />
+                      <input style={{position: "fixed", top: "95px"}} id="search" type="search" placeholder="Search..." autofocus required />
                       <h1 style={{color: "#fb5804", position: "fixed", left: "20px", top: "35px"}}>Search</h1>
                       <b><p>Coming Soon</p></b>
                 </body>
