6f0b5c38046e36bee3c165daa2c70381d43a609b
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-28 18:52:50 -0700

    Add files via upload

--- src/components/functions/Form.js
+++ src/components/functions/Form.js
@@ -5,7 +5,7 @@ export default function Form() {
     <form name="contact" method="post">
       <input type="hidden" name="form-name" value="contact" />
       <p>
-        <label htmlFor="name">Name</label> <br />
+        <label htmlFor="name">UserName</label> <br />
         <input type="text" id="name" name="name" required />
       </p>
       <p>
@@ -13,11 +13,11 @@ export default function Form() {
         <input type="email" id="email" name="email" required />
       </p>
       <p>
-        <label htmlFor="message">Message</label> <br />
-        <textarea id="message" name="message" required></textarea>
+        <label htmlFor="password">Password</label> <br />
+        <input id="password" name="password" required></input>
       </p>
       <p>
-        <input type="submit" value="Submit message" />
+        <input type="submit" value="Submit password" />
       </p>
     </form>
   );
