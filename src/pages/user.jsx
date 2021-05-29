58e60073a78e8f035f56b0cfd8b8036ad8c36069
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-29 00:46:29 -0700

    Add files via upload

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -11,7 +11,6 @@ import dm from '../components/images/dm.svg'
 
 /*CSS Imports*/
 import '../components/css/App.css';
-import Form from "../components/functions/Form";
 
 /*REACT START*/
 const UserPage = () => {
@@ -35,7 +34,6 @@ const UserPage = () => {
                 <br/>
                 <h1 style={{color: "#fb5804", position: "fixed", left: "20px", top: "35px"}}>User</h1>
                 <h3>Sign Up</h3>
-                <Form id="login-form"/>
           </body>
         </div>
         
