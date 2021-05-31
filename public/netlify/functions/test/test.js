c3079f269065c3014382df978277dd9bc0231868
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-31 00:56:44 -0700

    Delete public/netlify/functions/test directory

--- public/netlify/functions/test/test.js
+++
@@ -1,9 +0,0 @@
-const GoTrue = require("gotrue-js");
-
-auth = new GoTrue({
-  APIUrl: 'https://ivry.tk/.netlify/identity',
-  audience: '',
-  setCookie: true,
-});
-
-const user = auth.currentUser();
