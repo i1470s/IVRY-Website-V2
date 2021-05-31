24ed0c857540e75ea4170b72cd786bf639f648d1
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-05-31 01:06:12 -0700

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://api-ivry.herokuapp.com/api/test/";
+const API_URL = "https://api-ivry.herokuapp.com/api/test/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
