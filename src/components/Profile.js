b2d5c8cc3ffe33422ffc5c10d2e39b9bedb06a1a
Brayden Viana <i1470s@users.noreply.github.com>
2021-05-31 23:01:44 -0700

    Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -69,7 +69,7 @@ const Profile = () => {
         {currentUser.roles &&
           currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
       </ul>
-      <a style={{border: "2px #000 solid", borderRadius: "5px"}} href="/" onClick={logOut}>Log Out</a>
+      <a style={{border: "2px #fff solid", borderRadius: "5px"}} href="/" onClick={logOut}>Log Out</a>
       
       <div id="Footer">
         <footer className="App-footer">
