026c8b39089993d97b657517c22b1a248272a98d
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-06-07 01:10:03 -0700

    Add files via upload

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,5 +1,6 @@
 /*Imports*/
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 /*Image Imports*/
 import home from './images/home.svg';
@@ -10,11 +11,11 @@ import learn from './images/learn.svg';
 /*CSS Imports*/
 import './css/styles.scss';
 
-
 /*REACT START*/
 
 
 const Store= () => {
+const { user: currentUser } = useSelector((state) => state.auth);
     return (
        <div id="Page" className="App">
             
@@ -35,28 +36,20 @@ const Store= () => {
                       <br/>
                       <h1 style={{color: "#fb5804", position: "fixed", left: "20px", top: "35px"}}>Store</h1>
 
-                      <h4 style={{color: "#fb5804"}}>GTAV Recoverys</h4>
+                      <h3 style={{color: "#fff"}}>Welcome {currentUser.username}</h3>
                       
-                      <div class="store-row">
-                      <h4 class="store-list">Mini Recovery <br/><br/><br/> <b>$5</b> <br/><br/> <a class="store-button" data-sellix-product="5f63fa2d9728c" type="submit">Purchase</a></h4>
-                      <h4 class="store-list">Basic Recovery <br/><br/><br/> <b>$10</b> <br/><br/> <a class="store-button" data-sellix-product="5f63fb45a9771" type="submit">Purchase</a></h4>
-                      <h4 class="store-list">Pro Recovery <br/><br/><br/> <b>$15</b> <br/><br/> <a class="store-button" data-sellix-product="5f63fc353553c" type="submit">Purchase</a></h4>
-                      </div>
-                      <br/>
-
-                      <div class="store-row">
-                      <h4 class="store-list">Crazy Recovery <br/><br/><br/> <b>$20</b> <br/><br/> <a class="store-button" data-sellix-product="5f63fc365e6b5" type="submit">Purchase</a></h4>
-                      <h4 class="store-list">Insane Recovery <br/><br/><br/> <b>$25</b> <br/><br/> <a class="store-button" data-sellix-product="5f63fc3a22742" type="submit">Purchase</a></h4>
-                      <h4 class="store-list">Modded Account <br/><br/><br/> <b>$30</b> <br/><br/> <a class="store-button" data-sellix-product="5f63fd02368cc" type="submit">Purchase</a></h4>
-                      </div>
-                      <br/>
+                      <div class="list-shop"><h4>GTAV Recoverys</h4> <p>Stock: Varies</p><p>Price: $5 - $30</p> <a href="#"><button style={{color: "white", background: "#000", border: "2px #fff solid", borderRadius: "5px"}}>Options</button></a></div>
+                      <div class="list-shop"><h4>RDR2 Recoverys</h4> <p>Stock: Varies</p><p>Price: $10 - $50</p> <a href="#"><button style={{color: "white", background: "#000", border: "2px #fff solid", borderRadius: "5px"}}>Options</button></a></div> 
+                      <div class="list-shop"><h4>Discord Nitro</h4> <p>Stock: 0</p><p>Price: $5.50</p> <button style={{color: "white", background: "#000", border: "2px #fff solid", borderRadius: "5px"}} data-sellix-product="5f643a59f027a" type="submit">Purchase</button></div> 
+                      <div class="list-shop"><h4>Custom Discord Bot</h4> <p>Price: $25</p> <button style={{color: "white", background: "#000", border: "2px #fff solid", borderRadius: "5px"}} data-sellix-product="6083c9146e422" type="submit">Purchase</button></div>  
+                      <div class="list-shop"><h4>Hire Us</h4> <p>Price: Varies</p> <a href="https://linktr.ee/brayden_viana"><button style={{color: "white", background: "#000", border: "2px #fff solid", borderRadius: "5px"}}>Contact</button></a></div>
+                      <div class="list-shop"><h4>Donations</h4> <p>Price: Varies</p> <a href="https://www.paypal.com/donate?hosted_button_id=QWT56W6DFV8H4"><button style={{color: "white", background: "#000", border: "2px #fff solid", borderRadius: "5px"}}>Donate</button></a></div>
 
                       <h3 style={{color: "#fb5804"}}>Store Policy</h3>
                       <small>Welcome to our store, all our pices are in CAD(Canadian), we use sellix.io for secure and fast payment gateway for everyone of our products to help stop fraud, 
                       <br/>with over 30+ purchases in the past 3 months we think weve gained the trust of many 
                       <br/>and are able to show our selfs as fast, safe and lastly reliable,<br/>
                       please contact me on discord before any purchase i1470s#0396</small>
-
                 </body>
             </div>
             
@@ -71,6 +64,7 @@ const Store= () => {
                 </footer>
             </div>
         </div>
+        
     );
 };
 
