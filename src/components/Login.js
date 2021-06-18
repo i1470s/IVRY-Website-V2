e3b357eb902fee485e4655d77558fb090ec82a8f
Brayden Viana <55937162+i1470s@users.noreply.github.com>
2021-06-18 00:03:10 -0700

    Add files via upload

--- src/components/Login.js
+++ src/components/Login.js
@@ -70,7 +70,7 @@ const Login = (props) => {
     <div>
       <body className="App-body">
       <br/>
-      <h1 style={{color: "#fb5804", }}>IVRY</h1>
+      <h1 style={{color: "#fb5804"}}>IVRY</h1>
       <small style={{color: "white"}}>Log into your account below, or create a new one!</small>
 
       <div className="col-md-12">
